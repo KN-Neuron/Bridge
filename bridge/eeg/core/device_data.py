e9b6e995c7b145b588bddef9a27deb568dcb61c0
Mateusz <kowalski.mateusz.1lo1@gmail.com>
2026-05-18 23:01:31 +0200

    feat: eeg recording

--- bridge/eeg/core/device_data.py
+++ bridge/eeg/core/device_data.py
@@ -1,10 +1,18 @@
 from dataclasses import dataclass
 
+from bridge.eeg.core.typing import EEGArray
 
-@dataclass
+
+@dataclass(frozen=True, slots=True, kw_only=True)
 class DeviceData:
     mac_address: str | None = None
     name: str | None = None
     manufacturer: str | None = None
     electrodes_num: int | None = None
     sample_rate: int | None = None
+
+
+@dataclass(frozen=True, slots=True, kw_only=True)
+class RecordingFrame:
+    timestamp: float
+    data: EEGArray
